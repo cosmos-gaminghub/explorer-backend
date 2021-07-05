37dbf9db5cbdac7be46dd40711cc59a638f92cde
padi-dev-hoangpa <hoangpa@paditech.com>
2021-07-05 15:05:49 +0700

    Update schema

--- schema/block.go
+++ schema/block.go
@@ -6,24 +6,24 @@ import (
 
 // Block defines the structure for block information.
 type Block struct {
-	Height     int64     `bson:"height,omitempty"`
-	Proposer   string    `bson:"proposer,omitempty"`
-	Moniker    string    `bson:"moniker,omitempty"`
-	BlockHash  string    `bson:"block_hash,omitempty"`
-	ParentHash string    `bson:"parent_hash,omitempty"`
-	NumTxs     int64     `bson:"num_txs"`
-	Timestamp  time.Time `bson:"timestamp"`
+	ChainId   string    `bson:"chain_id"`
+	Height    int64     `bson:"height,omitempty"`
+	Proposer  string    `bson:"proposer,omitempty"`
+	Moniker   string    `bson:"moniker,omitempty"`
+	BlockHash string    `bson:"block_hash"`
+	NumTxs    int64     `bson:"num_txs"`
+	Timestamp time.Time `bson:"timestamp"`
 }
 
 // NewBlock returns a new Block.
 func NewBlock(b Block) *Block {
 	return &Block{
-		Height:     b.Height,
-		Proposer:   b.Proposer,
-		Moniker:    b.Moniker,
-		BlockHash:  b.BlockHash,
-		ParentHash: b.ParentHash,
-		NumTxs:     b.NumTxs,
-		Timestamp:  b.Timestamp,
+		ChainId:   b.ChainId,
+		Height:    b.Height,
+		Proposer:  b.Proposer,
+		Moniker:   b.Moniker,
+		BlockHash: b.BlockHash,
+		NumTxs:    b.NumTxs,
+		Timestamp: b.Timestamp,
 	}
 }
