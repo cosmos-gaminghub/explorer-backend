f217e89061fad929192afa9748db3b6593ea342a
padi-dev-hoangpa <hoangpa@paditech.com>
2021-07-07 16:04:23 +0700

    Update price sync

--- orm/document/stats_asset.go
+++ orm/document/stats_asset.go
@@ -14,9 +14,9 @@ const (
 
 // StatAssetInfoList1H defines the schema for asset statistics in an hourly basis
 type StatAssetInfoList20Minute struct {
-	Price     float32   `bson:"price"`
-	Marketcap float32   `bson:"market_cap"`
-	Volume24H float32   `bson:"volumne_24h"`
+	Price     float64   `bson:"price"`
+	Marketcap float64   `bson:"market_cap"`
+	Volume24H float64   `bson:"volumne_24h"`
 	Timestamp time.Time `bson:"timestamp"`
 }
 
--- schema/stats_asset.go
+++ schema/stats_asset.go
@@ -1,11 +1,13 @@
 package schema
 
-import "time"
+import (
+	"time"
+)
 
 // StatAssetInfoList1H defines the schema for asset statistics in an hourly basis
 type StatAssetInfoList20Minute struct {
-	Price     float32   `bson:"price"`
-	Marketcap float32   `bson:"market_cap"`
-	Volume24H float32   `bson:"volumne_24h"`
+	Price     float64   `bson:"price"`
+	Marketcap float64   `bson:"market_cap"`
+	Volume24H float64   `bson:"volumne_24h"`
 	Timestamp time.Time `bson:"timestamp"`
 }
